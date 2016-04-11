43dbc4271e47157fc55640bf5aa3ea4032c1b838
Pontus Ridderström <ponrid-2@student.ltu.se>
2016-04-11 15:58:17 +0200

    Removed default

--- E7012E-car-project/default/dep/main.o.d
+++
@@ -1,7 +0,0 @@
-main.o: ../main.c \
-  C:\Users\Admin\Documents\GitHub\E7012E-Car-Project\E7012E-car-project\PID/PID.h \
-  C:\Users\Admin\Documents\GitHub\E7012E-Car-Project\E7012E-car-project\MCU_Controller/MCU_Controller.h
-
-C:\Users\Admin\Documents\GitHub\E7012E-Car-Project\E7012E-car-project\PID/PID.h:
-
-C:\Users\Admin\Documents\GitHub\E7012E-Car-Project\E7012E-car-project\MCU_Controller/MCU_Controller.h:
--- E7012E-car-project/default/dep/resourceManager.o.d
+++
@@ -1,1 +0,0 @@
-resourceManager.o: ../resourceManager.c
