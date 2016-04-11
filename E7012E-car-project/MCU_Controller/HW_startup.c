93045085b56b088148b2b49a1ec94fe76d63bf87
Pontus Ridderström <ponrid-2@student.ltu.se>
2016-04-11 16:20:36 +0200

    Fixed failed comments

--- E7012E-car-project/MCU_Controller/HW_startup.c
+++ E7012E-car-project/MCU_Controller/HW_startup.c
@@ -3,12 +3,12 @@ void gpio(){
 
 }
 
-#PB6 timer1/counter1 AF:OC1B (PWM)
+//PB6 timer1/counter1 AF:OC1B (PWM)
 void motorPWM(){
 
 }
 
-#PB4 timer2/counter2 AF:OC2A (PWM) 
+//PB4 timer2/counter2 AF:OC2A (PWM) 
 void wheelPWM(){
 
 }
