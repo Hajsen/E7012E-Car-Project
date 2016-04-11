f440d293d036c2f806fe1f199e41691a47bd6607
Emkai <furchas@gmail.com>
2016-04-11 15:33:07 +0200

    PID-Init - added files and a resourceManager
    Added files for PID controller and added a resourceManager to be used if
    needed

--- E7012E-car-project/main.c
+++ E7012E-car-project/main.c
@@ -3,12 +3,25 @@
 
 int main()
 {
+	// startup process
 	startup();
+
+	// should be last thing called before main loop
+	run();
 	while(1);
 }
 
 
+// Calls all startup methods that need to be run before application runs.
+// Allocate resources etc...
 void startup()
 {
-	
+	PID_startup();
+}
+
+
+// Called when all setup and settings has been made
+void run()
+{
+	PID_run();
 }
