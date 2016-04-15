1aa275d18896f1c2074a2998e806bcca16c64fd4
Fredrik Nordin <efioni-2@student.ltu.se>
2016-04-15 13:27:17 +0200

    Change names and add bitmanipulated struct

--- E7012E-car-project/resourceManager.h
+++ E7012E-car-project/resourceManager.h
@@ -5,32 +5,36 @@
 #ifndef SENSOR_MESSAGE
 #define SENSOR_MESSAGE
 
-struct sensor_sessage
+void resourceManager_startup();
+
+typedef struct// SensorStatus
 { 
-    int outer_left;
-	int outer_right;
-	int sensor_left1;
-	int sensor_left2;
-	int sensor_left3;
-	int sensor_left4;
-	int sensor_left5;
-	int sensor_left6;
+    int outer_left : 1;
+	int outer_right : 1;
+	
+	int sensor_left1 : 1;
+	int sensor_left2 : 1;
+	int sensor_left3 : 1;
+	int sensor_left4 : 1;
+	int sensor_left5 : 1;
+	int sensor_left6 : 1;
 
-	int sensor_middle1;
-	int sensor_middle12;
-	int sensor_middle13;
-	int sensor_middle14;
-	int sensor_middle15;
-	int sensor_middle16;
+	int sensor_middle1 : 1;
+	int sensor_middle2 : 1;
+	int sensor_middle3 : 1;
+	int sensor_middle4 : 1;
+	int sensor_middle5 : 1;
+	int sensor_middle6 : 1;
 
-	int sensor_right1;
-	int sensor_right2;
-	int sensor_right3;
-	int sensor_right4;
-	int sensor_right5;
-	int sensor_right6;
-}sensor_sessage;
+	int sensor_right1 : 1;
+	int sensor_right2 : 1;
+	int sensor_right3 : 1;
+	int sensor_right4 : 1;
+	int sensor_right5 : 1;
+	int sensor_right6 : 1;
+} SensorStatus;
+
+SensorStatus current_status;
+SensorStatus last_status;
 
 #endif
-
-void resourceManager_startup();
