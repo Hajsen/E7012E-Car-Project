ea8a5e955e9ef41000311da062e4ffa319c12acf
Emkai <furchas@gmail.com>
2016-04-18 09:40:10 +0200

    Added PID function

--- E7012E-car-project/PID/PID.c
+++ E7012E-car-project/PID/PID.c
@@ -1,11 +1,76 @@
+#include <PID.h>
+
+#include "math.h"
+
+static float const Ki = 0.3;
+static float const Kp = 0.3;
+static float const Kd = 0.3;
+static int   const EPSILON = 1;
+static float const MAX = 10;
+static float const MIN = 0;
+static float const dt = 0.01;
+
 // allocate resources 
 void PID_startup()
 {
-	return;
 }
 
 // run controller
 void PID_run()
 {
-	return;
+	while(1)
+	{
+		// wait for mutex to be unlocked after "dt" time
+		calculateOutput(0, getPosition());
+	}
+}
+
+int getPosition()
+{
+	// read data from 
+}
+
+float calculateOutput(int reference, int position)
+{
+	// declare variables
+	static float pre_error;
+	static float integral;
+	float derivative;
+	float error;
+	float output;
+	
+	// if not is number set to 0
+	if(!isnan(pre_error))
+	{
+		pre_error = 0;
+	}
+	if(!isnan(integral))
+	{
+		integral = 0;
+	}
+	
+	// calculate error
+	error = reference - position;
+
+	// only calculate integral if error is large enough
+	if(error > EPSILON)
+	{
+		integral = integral + error * dt;
+	}
+	derivative = ( error - pre_error ) / dt;
+	output = Kp*error + Ki*integral + Kd * derivative;
+
+	// Keep output within allowed range
+	if(output > MAX)
+	{
+		output = MAX;
+	}
+	else if(output < MIN)
+	{
+		output = MIN;
+	}
+
+	pre_error = error;
+
+	return output;
 }
---
+++ E7012E-car-project/PID/PID.h
@@ -0,0 +1,10 @@
+#ifndef PID_CAR_CONTROLLER
+#define PID_CAR_CONTROLLER
+
+void PID_startup();
+void PID_run();
+float calculateOutput();
+
+
+
+#endif
