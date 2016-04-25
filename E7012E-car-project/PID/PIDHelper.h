9ff77a9e91533148f7e805681438789234418959
william mokhlef <Wilmok-2@student.ltu.se>
2016-04-25 16:38:54 +0200

    car velocity tracker

---
+++ E7012E-car-project/PID/PIDHelper.h
@@ -0,0 +1,8 @@
+#define static int OVERFLOW_COUNTER = 0;
+#define static int TIME_BEFORE_OVERFLOW = 0;
+#define static int TIME_PREVIOUS = 0;
+#define static float VELOCITY;
+
+#define int MAX_INT 0xFFFF;
+#define int RADIUS = 23;
+#define float distance = (2*M_PI*RADIUS)/6;
